a8e3dd1c19451b00951dec8431b89d6971e1c64d
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-11-03 12:23:25 +0100

    Initial commit

---
+++ app/src/main/java/www/iesmurgi/u5animacionnbrealumno/MainActivity.kt
@@ -0,0 +1,33 @@
+package www.iesmurgi.u5animacionnbrealumno
+
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Instacia de TextView de texto de xml
+        var miTexto:TextView = findViewById(R.id.texto);
+        miTexto.setText("He cambiado el texto");
+
+        //Cambiamos color
+        miTexto.setTextColor(getResources().getColor(R.color.miColor));
+
+        //Añadimos texto al texto
+        miTexto.append("\n Nueva línea de texto");
+
+        //Cambiamos la tipografia
+        miTexto.typeface = ResourcesCompat.getFont(this, R.font.umbrella);
+
+        val mianimacion = AnimationUtils.loadAnimation(this, R.anim.animacion_translacion);
+        miTexto.startAnimation(mianimacion);
+
+    }
+}
